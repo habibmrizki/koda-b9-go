9c6b3af99489ad3309404f4a514d795abbadefb5
habibmrizki <habibboboho@gmail.com>
2026-09-07 21:58:02 +0700

    feat(slice): implement middle element insertion for integer slice

---
+++ internal/slice/slices.go
@@ -0,0 +1,11 @@
+package slice
+
+func SlicesNumbers(originalSlice []int) []int {
+	valueToInsert := 88
+	// valueToInsert := 88
+	// valueToInsert := []int{88}
+	// insertIndex := 3
+	mid := len(originalSlice) / 2
+	return append(append(originalSlice[:mid:mid], valueToInsert), originalSlice[mid:]...)
+	// 	return append(append(originalSlice[:insertIndex:insertIndex], valueToInsert), originalSlice[insertIndex:]...)
+}
